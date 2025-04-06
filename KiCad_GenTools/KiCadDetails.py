5362acd6a209f68f759403892a225419c5b6ddb6
Romzes <44292649+RomanPorazinski@users.noreply.github.com>
2025-04-06 18:09:22 +0100

    Extracting the name of 'return'
    Added a function and implemented the call of outputName to extract the name of passed output.
    
    Also added blockSize function which will return the height of KiCad block based on the number of inputs/outputs

--- KiCad_GenTools/KiCadDetails.py
+++ KiCad_GenTools/KiCadDetails.py
@@ -1,4 +1,6 @@
 ## Class containing all details that KiCad needs to generate symbol
+import ast
+import inspect
 class PopulateKiCadDetails:
     def __init__(self):
         self.KiCadDetails = {
@@ -16,3 +18,39 @@ class PopulateKiCadDetails:
         details["RectangleEndY"] = EdgeOffset*2 + InputCount*InputSpacing
 
         return details
+
+    def blockSize(self, InputCount, OutputCount):
+        portCount = max(InputCount, OutputCount)
+        EdgeOffset = 1.27
+        InputSpacing = 2.54
+        details = {"RectangleEndX": 29.21, "RectangleEndY":0}
+
+        details["RectangleEndY"] = EdgeOffset*2 + InputCount*InputSpacing
+
+        return details
+
+    def outputName(self, funcName):
+        functionSource = inspect.getsource(funcName)
+        tree = ast.parse(functionSource)
+        returnName = 0
+        for node in ast.walk(tree):
+            # Traverse the called function (line by line)
+
+            if isinstance(node, ast.Return):
+                # Line with the Return function is found
+                # Log the variable defined as Return
+                returnDetected = node.value
+
+                # Check the data type of the Return
+                if isinstance(returnDetected, ast.Name):
+                    returnName = returnDetected.id
+
+                elif isinstance(returnDetected, ast.BinOp):
+                    returnName = ast.dump(returnDetected, indent=4)
+
+                else:
+                    print("Return value type:", type(returnDetected).__name__)
+                    print(ast.dump(returnDetected))
+                    returnName = 0
+
+        return returnName
--- KiCad_GenTools/PythonToKiCad.py
+++ KiCad_GenTools/PythonToKiCad.py
@@ -11,6 +11,15 @@ for name, func in inspect.getmembers(SampleFunctions, inspect.isfunction):
     # Define empty dict
     functionDetails = {}
 
+    # Log the function name
+    functionDetails["Name"] = list({name})[0] # This logs the actual string
+    print(functionDetails["Name"])
+    #print(functionDetails["Name"][0]) # Adding the [0] prints the actual string, which is the first one in the list
+
+
+
+    DM = PopulateKiCadDetails() # Initiate instance of the module
+
     # Extract information from the function
     signature = inspect.signature(func)
     params = signature.parameters
@@ -28,6 +37,12 @@ for name, func in inspect.getmembers(SampleFunctions, inspect.isfunction):
     exportedList = list(params.values())
     print(functionDetails["inputList"][0])
     print(functionDetails["inputCount"])
+
+    print("test of hidden function")
+    returnFunctionName = DM.outputName(func)
+    print(returnFunctionName)
+
+
 
     # Populate other function-specific details for KICad gen
 
